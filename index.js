25ba8c6f5e4c8095e151558aa6bf267c429ba037
Jeremy Woffinden <jeremywoffinden25@gmail.com>
2019-10-20 17:23:56 +0900

    react and express set up

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => {
+// Test route
+app.get('/api/v1', (req,res) => {
     res.send({hello:"world"});
 });
 
+// Making sure routing works in prod
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+// Switch ports between dev and prod
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
