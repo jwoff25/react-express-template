7f6933493ab9357af38189c403c45cd9ce52a9e4
Jeremy Woffinden <jeremywoffinden25@gmail.com>
2020-03-03 23:50:54 +0900

    saved current progress to separate branch for backup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 // Import reducers
 import alert from "./alert";
 import auth from "./auth";
+import profile from "./profile";
 
 // Export all reducers in one fell swoop
 export default combineReducers({
     alert,
-    auth
+    auth,
+    profile
 });
